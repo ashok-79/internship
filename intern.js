2a63028df80bd1e83bddf55c3ddc3bcf76fdfa58
ashok-hb <110151417+ashok-hb@users.noreply.github.com>
2023-11-17 15:28:50 +0530

    Update intern.js

--- intern.js
+++ intern.js
@@ -1,1 +1,6 @@
-
+function submitFeedback() {
+    const recommendation = document.querySelector('input[name="recommendation"]:checked');
+    const suggestion = document.getElementById('suggestionBox').value;
+    const rating = document.getElementById('productRating').value;
+    alert(`Thank you for your feedback!\nRecommendation: ${recommendation ? recommendation.value : 'Not provided'}\nSuggestion: ${suggestion}\nRating: ${rating}`);
+}
